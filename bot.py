2e957315dfbd6eb46b2bd7bf0e792a28c34b9099
smitjethwa <smitjethwa20@gmail.com>
2020-07-17 02:59:33 +0530

    function added

--- bot.py
+++ bot.py
@@ -7,17 +7,15 @@ from firebase_admin import credentials
 from firebase_admin import db
 import os
 
-TOKEN = " " # Telegram Authorization Token
+TOKEN = '1361748338:AAEG53stqIhZUn5sA5WTA-qY_XyeH-5LdsU'
 ADMINS = ['smitjethwa']
-FIREBASE_TOKEN = " " # FIREBASE_TOKEN
-APP_URL = "https://yourherokuappname.herokuapp.com/" # Heroku App URL
-
+FIREBASE_TOKEN = "hey-smit-firebase-adminsdk-52vpd-86c5806a19.json"
 PORT = int(os.environ.get('PORT', 5000))
 
 cred = credentials.Certificate(FIREBASE_TOKEN)
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'http://<project-id.firebaseio.com/', # Database URL 
+    'databaseURL': 'https://tfug-mumbai.firebaseio.com',
     'databaseAuthVariableOverride': None
 })
 
@@ -206,7 +204,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook(APP_URL + TOKEN)
+    updater.bot.setWebhook('https://tfugmumbai-bot.herokuapp.com/' + TOKEN)
     updater.idle()
     
 if __name__ == '__main__':
