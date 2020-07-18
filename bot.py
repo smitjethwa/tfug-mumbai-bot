568904cf8a3ef223662e4ae52b5f8f0dae44a1a7
Smit Jethwa <smitjethwa20@gmail.com>
2020-07-18 15:11:42 +0530

    get_details() minimized

--- bot.py
+++ bot.py
@@ -32,79 +32,25 @@ COURSE_EMOJI = u'\U0001F4D9'
 EVENT_EMOJI = u'\U0001F3A4'
 
 def list_to_string(message_list):
+    """Prepare a single String Message from List Elements."""
     return "\n\n".join(message_list) 
 
-def get_blog():
-    ref = db.reference('/admin/blogs/').get()
-    dict_key = []
-    dict_value = []
+def get_details(category):
+    """This Function retrieves the data from Firebase Realtime Database
+    category - name of the child. For eg. blogs, events etc. 
+    """
+    ref = db.reference(f'/admin/{category}/').get()
+    temp_value = []
+    details_list = []
     for x in list(ref)[0:]:
-        dict_key.append(x)
-        dict_value.append(ref[x])
-    # return dict_key,dict_value 
-    complete_list = []
-    for i in range(len(dict_value)):
-        individual_list = []
-        individual_list.append(dict_value[i]['blog_title'])
-        individual_list.append(dict_value[i]['blog_url'])
-        complete_list.append(individual_list)
-    return complete_list
-
-def get_tutorial():
-    ref = db.reference('/admin/tutorials/').get()
-    dict_key = []
-    dict_value = []
-    for x in list(ref)[0:]:
-        dict_key.append(x)
-        dict_value.append(ref[x])
-    # return dict_key,dict_value 
-    complete_list = []
-    for i in range(len(dict_value)):
-        individual_list = []
-        individual_list.append(dict_value[i]['tutorial_platform'])
-        individual_list.append(dict_value[i]['tutorial_title'])
-        individual_list.append(dict_value[i]['tutorial_url'])
-        complete_list.append(individual_list)
-    return complete_list
-
-def get_course():
-    ref = db.reference('/admin/courses/').get()
-    dict_key = []
-    dict_value = []
-    for x in list(ref)[0:]:
-        dict_key.append(x)
-        dict_value.append(ref[x])
-    # return dict_key,dict_value 
-    complete_list = []
-    for i in range(len(dict_value)):
-        individual_list = []
-        individual_list.append(dict_value[i]['course_platform'])
-        individual_list.append(dict_value[i]['course_title'])
-        individual_list.append(dict_value[i]['course_url'])
-        complete_list.append(individual_list)
-    return complete_list
-
-def get_event():
-    ref = db.reference('/admin/events/').get()
-    dict_key = []
-    dict_value = []
-    for x in list(ref)[0:]:
-        dict_key.append(x)
-        dict_value.append(ref[x])
-    # return dict_key,dict_value 
-    complete_list = []
-    for i in range(len(dict_value)):
-        individual_list = []
-        individual_list.append(dict_value[i]['event_date'])
-        individual_list.append(dict_value[i]['event_link'])
-        individual_list.append(dict_value[i]['event_name'])
-        individual_list.append(dict_value[i]['event_location'])
-        complete_list.append(individual_list)
-    return complete_list
+        temp_value.append(ref[x])
+    for i in range(len(temp_value)):
+        details_list.append(list(temp_value[i].values()))
+    return details_list
 
 def start(bot, update):
-    user_first_name = update.effective_user.first_name
     """Send a message when the command /start is issued."""
+    user_first_name = update.effective_user.first_name
     message_to_be_send = f'<b>Hi {user_first_name}</b>! \nRead TensorFlow blogs via /blogs or explore more using /help'
     update.message.reply_text(message_to_be_send,parse_mode=telegram.ParseMode.HTML)
 
@@ -121,8 +67,8 @@ def help(bot, update):
     update.message.reply_text(message_to_be_send,parse_mode=telegram.ParseMode.HTML)
 
 def blogs(bot, update):
-    """Send a message when the command /help is issued."""
-    complete_list = get_blog()
+    """Send a message when the command /blogs is issued."""
+    complete_list = get_details('blogs')
     message_to_be_send = []
     for i in range(len(complete_list)):
         blog_title = complete_list[i][0]
@@ -132,8 +78,8 @@ def blogs(bot, update):
     update.message.reply_text(output_text,parse_mode=telegram.ParseMode.HTML)
   
 def tutorials(bot, update):
-    """Send a message when the command /help is issued."""
-    complete_list = get_tutorial()
+    """Send a message when the command /tutorials is issued."""
+    complete_list = get_details('tutorials')
     message_to_be_send = []
     for i in range(len(complete_list)):
         tutorial_platform = complete_list[i][0]
@@ -144,8 +90,8 @@ def tutorials(bot, update):
     update.message.reply_text(output_text,parse_mode=telegram.ParseMode.HTML)
 
 def events(bot, update):
-    """Send a message when the command /help is issued."""
-    complete_list = get_event()
+    """Send a message when the command /events is issued."""
+    complete_list = get_details('events')
     message_to_be_send = []
     for i in range(len(complete_list)):
         event_date = complete_list[i][0].split('-')
@@ -158,8 +104,8 @@ def events(bot, update):
     update.message.reply_text(output_text,parse_mode=telegram.ParseMode.HTML)
 
 def courses(bot, update):
-    """Send a message when the command /help is issued."""
-    complete_list = get_course()
+    """Send a message when the command /courses is issued."""
+    complete_list = get_details('courses')
     message_to_be_send = []
     for i in range(len(complete_list)):
         course_platform = complete_list[i][0]
@@ -170,7 +116,7 @@ def courses(bot, update):
     update.message.reply_text(output_text,parse_mode=telegram.ParseMode.HTML)
 
 def social_media(bot, update):
-    """Send a message when the command /help is issued."""
+    """Send a message when the command /social_media is issued."""
     message_to_be_send = '''
     Be a part of TFUG Mumbai.
     Telegram : <a href="https://t.me/tfugmumbai">@tfugmumbai</a>
@@ -181,7 +127,8 @@ def social_media(bot, update):
     '''
     update.message.reply_text(message_to_be_send,parse_mode=telegram.ParseMode.HTML)
 
-def echo(update, context):
+def echo(bot, update):
+    """Send a message when the unknown command is issued."""
     update.message.reply_text("Unsupported Command! Check /help")
 
 def error(update, context):
